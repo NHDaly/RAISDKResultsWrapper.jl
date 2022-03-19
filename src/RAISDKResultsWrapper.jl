efc0b501bd67dd9ca73f038c4416ea49c68ab805
Nathan Daly <nhdaly@gmail.com>
2022-03-19 09:45:34 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.26

---
+++ src/RAISDKResultsWrapper.jl
@@ -0,0 +1,5 @@
+module RAISDKResultsWrapper
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using RAISDKResultsWrapper
+using Test
+
+@testset "RAISDKResultsWrapper.jl" begin
+    # Write your tests here.
+end
